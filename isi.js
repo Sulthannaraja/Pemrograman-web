eeba05d1582606972f1d9793ff285eb00775105c
Sulthannaraja <sulthantegarnaraja@gmail.com>
2024-10-02 14:33:58 +0700

    Update isi.js

--- isi.js
+++ isi.js
@@ -1,4 +1,4 @@
-document.writen("<center>");
+document.writeln("<center>");
 document.writeln("<hr width=600 color=black>");
 document.writeln("<h1>UNIVERSITAS LAMPUNG</h1>");
 document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</h2>");
